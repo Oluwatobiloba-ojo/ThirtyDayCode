8e8105323c955aef7b8568ed2638292334d3e0ed
Ojo Oluwatobi <ojot630@gmail.com>
2024-02-02 09:10:46 +0100

    first day commit

--- src/ReversePolishNotation.java
+++ src/ReversePolishNotation.java
@@ -1,5 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReversePolishNotation {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static int evalRPM(String[] input) {
+        List<String> result = new ArrayList<>();
+        for (int count = 0; count < input.length; count++){
+            if (input[count].equals("-") || input[count].equals("+") || input[count].equals("*") || input[count].equals("/") || input[count].equals("%")){
+                result.add(input[count]);
+                solveExpression(input[count], result);
+            }
+            else result.add(input[count]);
+        }
+        return Integer.parseInt(result.get(0));
     }
+
+    private static void solveExpression(String value, List<String> result) {
+        int indexOfOperator = result.indexOf(value);
+        int number = 0;
+        String firstValueOfOperator = result.remove(indexOfOperator - 1);
+        String secondValueOfOperator = result.remove(indexOfOperator - 2);
+        int firstValue = changeValueToInt(firstValueOfOperator);
+        int secondValue = changeValueToInt(secondValueOfOperator);
+        if (value.equals("+")) number = secondValue + firstValue;
+        else if (value.equals("-")) number = secondValue - firstValue;
+        else if (value.equals("*")) number = secondValue * firstValue;
+        else if (value.equals("/")) number = secondValue / firstValue;
+        else if (value.equals("%")) number = secondValue % firstValue;
+        result.remove(value);
+        result.add(String.valueOf(number));
+    }
+
+    private static int changeValueToInt(String firstValueOfOperator) {
+        if (!firstValueOfOperator.contains("-")) return Integer.parseInt(firstValueOfOperator);
+        else{
+            String copiedValue = "";
+            for (int index = 1; index < firstValueOfOperator.length() ; index++) {
+                copiedValue += firstValueOfOperator.charAt(index);
+            }
+            return -Integer.parseInt(copiedValue);
+        }
+    }
+
 }
--- test/ReversePolishNotationTest.java
+++ test/ReversePolishNotationTest.java
@@ -1,4 +1,29 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ReversePolishNotationTest {
-  
+
+    @Test
+    public void testThatTheReversePolishNotationOfThisInputReturn9(){
+        String [] input = {"2","1","+","3","*"};
+        assertEquals(9, ReversePolishNotation.evalRPM(input));
+    }@Test
+    public void testThatTheReversePolishNotationOfThisInputReturn22(){
+        String [] input = {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
+        assertEquals(22, ReversePolishNotation.evalRPM(input));
+    }@Test
+    public void testThatTheReversePolishNotationOfThisInputReturn1(){
+        String [] input = {"4", "3", "-"};
+        assertEquals(1, ReversePolishNotation.evalRPM(input));
+    }@Test
+    public void testThatTheReversePolishNotationOfThisInputReturn7(){
+        String [] input = {"4", "-3", "-"};
+        assertEquals(7, ReversePolishNotation.evalRPM(input));
+    }@Test
+    public void testThatTheReversePolishNotationOfThisInputReturnMinus1(){
+        String [] input = {"4", "-3", "-", "8", "-"};
+        assertEquals(-1, ReversePolishNotation.evalRPM(input));
+    }
+
 }
