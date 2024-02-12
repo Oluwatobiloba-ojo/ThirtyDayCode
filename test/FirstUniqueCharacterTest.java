85e57d582b3751a9aa0d56d100d5700abe901a1a
Ojo Oluwatobi <ojot630@gmail.com>
2024-02-12 03:45:39 +0100

    second day

--- test/FirstUniqueCharacterTest.java
+++ test/FirstUniqueCharacterTest.java
@@ -1,4 +1,22 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FirstUniqueCharacterTest {
-  
+
+    @Test
+    public void testThatTheUniqueCharacterInLeet_CodeTheIndexIs0(){
+        String value = "leetcode";
+        assertEquals(0, FirstUniqueCharacter.firstUniqueChar(value));
+    }@Test
+    public void testThatTheUniqueCharacterIn_aabbTheIndexIsMinusOne(){
+        String value = "aabb";
+        assertEquals(-1, FirstUniqueCharacter.firstUniqueChar(value));
+    }@Test
+    public void testThatTheUniqueCharacterIn_loveleetcodeTheIndexIs2(){
+        String value = "loveleetcode";
+        assertEquals(2, FirstUniqueCharacter.firstUniqueChar(value));
+    }
+
+
 }
