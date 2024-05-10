9973a5e82006959914421835fa019484139bbfbc
Ojo Oluwatobi <ojot630@gmail.com>
2024-05-11 00:18:10 +0100

    committed SmallestPrimeFactor.java refactored

--- src/SmallestPrimeFactor.java
+++ src/SmallestPrimeFactor.java
@@ -10,26 +10,24 @@ public class SmallestPrimeFactor {
         for (int count = 0; count < numbers.length; count++){
             getFraction(count, numbers, numbers[count], values);
         }
-        String[] results = values.get(constant).split("/");
+        String[] results = values.get(constant-1).split("/");
         System.out.println(values);
         result[0] = Integer.parseInt(results[0]);
         result[1] = Integer.parseInt(results[1]);
         return result;
     }
 
-    private static List<String> getFraction(int index, int[] numbers, int value, List<String> values) {
+    private static void getFraction(int index, int[] numbers, int value, List<String> values) {
         for (int count = numbers.length-1; count > index; count-- ){
-            if(count == numbers.length && !values.isEmpty()){
+            if(count == numbers.length-1 && !values.isEmpty()){
                 String[] former = values.get(values.size()-1).split("/");
-                if (Integer.parseInt(former[0])/Integer.parseInt(former[1]) > value / count){
-                    values.add(values.size()-1, value+"/"+count);
-                }else values.add(value+"/"+count);
+                double val = (double) Integer.parseInt(former[0]) / Integer.parseInt(former[1]);
+                if (val > (double) value / numbers[count]){
+                    values.add(values.size()-1, value+"/"+numbers[count]);
+                }else values.add(value+"/"+numbers[count]);
                 continue;
             }
-            values.add(value+"/"+count);
+            values.add(value+"/"+numbers[count]);
         }
-        return values;
     }
-
-
 }
--- test/SmallestPrimeFactorTest.java
+++ test/SmallestPrimeFactorTest.java
@@ -6,9 +6,15 @@ class SmallestPrimeFactorTest {
 
     @Test
     public void testThatSmallestPrimeFactorCanInThisArrayIsTwoAndFive(){
-        int[] numbers = {1, 2, 3, 4, 5};
+        int[] numbers = {1, 2, 3, 5};
         int constant = 3;
         int[] result = {2, 5};
+        assertArrayEquals(result, SmallestPrimeFactor.kthSmallestPrimeFactor(numbers, constant));
+    } @Test
+    public void testThatSmallestPrimeFactorCanInThisArrayIsOneAndSeven(){
+        int[] numbers = {1, 7};
+        int constant = 1;
+        int[] result = {1, 7};
         assertArrayEquals(result, SmallestPrimeFactor.kthSmallestPrimeFactor(numbers, constant));
     }
 
