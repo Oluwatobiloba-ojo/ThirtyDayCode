2be32abd473cb48b8d4d9dc41659448826158570
Ojo Oluwatobi <ojot630@gmail.com>
2024-05-13 23:04:44 +0100

    added BuyTwoChocolate.java method on 13/05/2024

--- src/BuyTwoChocolate.java
+++ src/BuyTwoChocolate.java
@@ -4,7 +4,7 @@ public class BuyTwoChocolate {
 
     public static int butChoco(int[] prices, int money) {
         prices = Arrays.stream(prices).sorted().toArray();
-        if (prices[1] + prices[0] <= money) return (prices[1] + prices[0]) - money;
+        if (prices[1] + prices[0] <= money) return money - (prices[1] + prices[0]);
         else return money;
     }
 }
